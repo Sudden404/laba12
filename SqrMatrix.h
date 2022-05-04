8647af6fcdd14da6714e3494f9f97c71956df4b5
User <vlad00001234567890@mail.ru>
2022-05-04 11:15:59 +0300

    new file

--- SqrMatrix.h
+++ SqrMatrix.h
@@ -1,11 +1,6 @@
-//
-// Created by User on 02.05.2022.
-//
-
 #ifndef LABA12_SQRMATRIX_H
 #define LABA12_SQRMATRIX_H
 #include <iostream>
-
 
 class SqrMatrix {
 private:
@@ -25,9 +20,5 @@ public:
     void Set(const int&,const int&,const double&);
     void Div(SqrMatrix&, SqrMatrix&);
 };
-/*void inversion(double**, size_t);
-double Determinant(double**, size_t);
-void GetMatr(double**, double**, int i, int j, int m);
-void Print_mas(double**, int);*/
 
 #endif //LABA12_SQRMATRIX_H
---
+++ my.lib.h
@@ -0,0 +1,11 @@
+#ifndef LABA12_MY_LIB_H
+#define LABA12_MY_LIB_H
+//#include "SqrMatrix.h"
+#include <iostream>
+
+void inversion(double**, size_t);
+double Determinant(double**, size_t);
+void GetMatr(double**, double**, int i, int j, int m);
+void Print_mas(double**, int);
+
+#endif //LABA12_MY_LIB_H
